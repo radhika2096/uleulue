b395ecd51502bcfc5b4495375a0cc3f9dcb1e309
Klausdurjoy <83454101+Klausdurjoy@users.noreply.github.com>
2021-05-16 15:34:34 +0600

    hi

---
+++ app/src/main/java/com/example/uleulue/loginstudent.java
@@ -0,0 +1,14 @@
+package com.example.uleulue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class loginstudent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loginstudent);
+    }
+}
