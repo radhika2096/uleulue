d58beb1852f7ab67c54a06466d95d2f7b555b071
radhika2096 <sanghairadhika@gmail.com>
2021-05-19 23:59:31 +0545

    erer

---
+++ app/src/main/java/com/example/uleulue/loginwardenpade.java
@@ -0,0 +1,14 @@
+package com.example.uleulue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class loginwardenpade extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loginwardenpade);
+    }
+}
