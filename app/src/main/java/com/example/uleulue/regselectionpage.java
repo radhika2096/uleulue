661d764f0ff28e7b0a612f4b7e9607986c4e564e
radhika2096 <sanghairadhika@gmail.com>
2021-05-29 22:18:45 +0545

    reg

--- app/src/main/java/com/example/uleulue/regselectionpage.java
+++ app/src/main/java/com/example/uleulue/regselectionpage.java
@@ -2,13 +2,21 @@ package com.example.uleulue;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class regselectionpage extends AppCompatActivity {
+    public void changeUser(View view){
+        if(view.getTag().toString()=="students")
+            startActivity(new Intent(regselectionpage.this,registerStudentPage.class));
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_regselectionpage);
+
     }
 }
