a136e39dafb00141e44bb6178fd2900cfde1e918
radhika2096 <sanghairadhika@gmail.com>
2021-05-29 21:20:09 +0545

    reg

---
+++ app/src/main/java/com/example/uleulue/regselectionpage.java
@@ -0,0 +1,14 @@
+package com.example.uleulue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class regselectionpage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regselectionpage);
+    }
+}
