2fa28abf0d189d28d3803bd126653e75622d0a36
radhika2096 <sanghairadhika@gmail.com>
2021-05-30 13:13:49 +0530

    reg

---
+++ app/src/main/java/com/example/uleulue/login_activity_gatekeeper.java
@@ -0,0 +1,14 @@
+package com.example.uleulue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class login_activity_gatekeeper extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_gatekeeper);
+    }
+}
