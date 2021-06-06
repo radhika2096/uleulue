32f0b5e0d827e07d3bec2828cb12ffda6099577b
chetan9373 <61279954+chetan9373@users.noreply.github.com>
2021-06-06 13:48:10 +0530

    reg

--- app/src/main/java/com/example/uleulue/GatekeeperRegister.java
+++ app/src/main/java/com/example/uleulue/GatekeeperRegister.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Gatekeeperlogin extends AppCompatActivity {
+public class GatekeeperRegister extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_gatekeeperlogin);
+        setContentView(R.layout.activity_gatekeeper_register);
     }
 }
