40697f05c2a9a21b1c7845e7230792d2e03c727a
chetan9373 <61279954+chetan9373@users.noreply.github.com>
2021-07-06 22:05:08 +0530

    dgfhsrjjf

---
+++ app/src/main/java/com/example/uleulue/GatekeeperLogin.java
@@ -0,0 +1,14 @@
+package com.example.uleulue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GatekeeperLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gatekeeper_login);
+    }
+}
