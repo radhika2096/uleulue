98c4d2a8b2c0ac64219283a4615d9f351a4d3b6d
BlackFire <apoorvagarg9148@gmail.com>
2021-08-01 21:16:59 +0530

    yoyo

--- app/src/main/java/com/example/uleulue/studentsdhundoviawarden.java
+++ app/src/main/java/com/example/uleulue/studentsdhundoviawarden.java
@@ -9,8 +9,13 @@ import android.app.DownloadManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
@@ -26,14 +31,17 @@ import com.google.firebase.database.ValueEventListener;
 
 import org.jetbrains.annotations.NotNull;
 
-public class studentsdhundoviawarden extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class studentsdhundoviawarden extends AppCompatActivity implements Filterable {
     FirebaseRecyclerOptions<requestdhundo> options;
-    FirebaseRecyclerAdapter<requestdhundo,matapitadhundoHolder>adapter;
+    FirebaseRecyclerAdapter<requestdhundo, matapitadhundoHolder> adapter;
     DatabaseReference mUserRef;
     FirebaseAuth mAuth;
     FirebaseUser muser;
     RecyclerView recyclerView;
-
+    List<requestdhundo> listfull;
 
 
     @Override
@@ -49,49 +57,43 @@ public class studentsdhundoviawarden extends AppCompatActivity {
 
         mUserRef = FirebaseDatabase.getInstance().getReference().child("databaseofwarden");
 
-        loadUsers( "");
+        loadUsers("");
 
 
     }
 
 
-
-
     private void loadUsers(String s) {
 
-        Query query = mUserRef.orderByChild("address").startAt(s).endAt(s+ "\uf8ff");
-        options = new FirebaseRecyclerOptions.Builder<requestdhundo>().setQuery(query,requestdhundo.class).build();
+        Query query = mUserRef.orderByChild("address").startAt(s).endAt(s + "\uf8ff");
+        options = new FirebaseRecyclerOptions.Builder<requestdhundo>().setQuery(query, requestdhundo.class).build();
         adapter = new FirebaseRecyclerAdapter<requestdhundo, matapitadhundoHolder>(options) {
             @Override
             protected void onBindViewHolder(@NonNull @NotNull matapitadhundoHolder holder, int position, @NonNull @NotNull requestdhundo model) {
 
 
+                holder.username.setText(model.getName());
+                holder.profession.setText(model.getAddress());
+                holder.studentsname.setText(model.getExitDate());
 
+                holder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
 
+                        Intent intent = new Intent(studentsdhundoviawarden.this, wardenrequestpreviewpage.class);
+                        intent.putExtra("userkey5", getRef(position).getKey().toString());
+                        startActivity(intent);
 
-                    holder.username.setText(model.getName());
-                    holder.profession.setText(model.getAddress());
-                    holder.studentsname.setText(model.getExitDate());
-
-                    holder.itemView.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-
-                            Intent intent = new Intent(studentsdhundoviawarden.this, wardenrequestpreviewpage.class);
-                            intent.putExtra("userkey5", getRef(position).getKey().toString());
-                            startActivity(intent);
-
-                        }
-                    });}
-
-
+                    }
+                });
+            }
 
 
             @NonNull
 
             @Override
-            public matapitadhundoHolder onCreateViewHolder (@NonNull ViewGroup parent,
-                                                            int viewType){
+            public matapitadhundoHolder onCreateViewHolder(@NonNull ViewGroup parent,
+                                                           int viewType) {
                 View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_show_parents, parent, false);
 
 
@@ -105,7 +107,85 @@ public class studentsdhundoviawarden extends AppCompatActivity {
 
 
     }
+
+    @Override
+    public Filter getFilter() {
+        return null;
+    }
+
+
+
+    private void process(String s) {
+
+        Query query = mUserRef.orderByChild("address").startAt(s).endAt(s + "\uf8ff");
+        options = new FirebaseRecyclerOptions.Builder<requestdhundo>().setQuery(query, requestdhundo.class).build();
+        adapter = new FirebaseRecyclerAdapter<requestdhundo, matapitadhundoHolder>(options) {
+
+
+            @Override
+            protected void onBindViewHolder(@NonNull @NotNull matapitadhundoHolder holder, int position, @NonNull @NotNull requestdhundo model) {
+
+
+                holder.username.setText(model.getName());
+                holder.profession.setText(model.getAddress());
+                holder.studentsname.setText(model.getExitDate());
+
+                holder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        Intent intent = new Intent(studentsdhundoviawarden.this, wardenrequestpreviewpage.class);
+                        intent.putExtra("userkey5", getRef(position).getKey().toString());
+                        startActivity(intent);
+
+                    }
+                });
+            }
+
+
+            @NonNull
+
+            @Override
+            public matapitadhundoHolder onCreateViewHolder(@NonNull ViewGroup parent,
+                                                           int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_show_parents, parent, false);
+
+
+                return new matapitadhundoHolder(view);
+            }
+        };
+        adapter.startListening();
+        recyclerView.setAdapter(adapter);
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+
+        getMenuInflater().inflate(R.menu.searchmenu, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView.setQueryHint("Type here to search");
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                process("");
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                process("");
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
 }
+
+
+
 
 
 
--- app/src/main/java/com/example/uleulue/wardenKiHistory.java
+++ app/src/main/java/com/example/uleulue/wardenKiHistory.java
@@ -66,27 +66,5 @@ public class wardenKiHistory extends AppCompatActivity {
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
 
-
-        getMenuInflater().inflate(R.menu.searchmenu,menu);
-        MenuItem menuItem=menu.findItem(R.id.action_search);
-        SearchView searchView=(SearchView) menuItem.getActionView();
-        searchView.setQueryHint("Type here to search");
-
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                adapter.getFilter().filter(newText);
-                return false;
-            }
-        }) ;
-        return super.onCreateOptionsMenu(menu);
-    }
 }
